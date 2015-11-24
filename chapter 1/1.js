fb1815661fd7db346baac528d6a3b02193e97fc0
berwin <berwin1995@qq.com>
2015-11-24 18:21:08 +0800

    update

---
+++ chapter 1/1.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var check = {
+  checkName: function() {
+    // 验证姓名
+  },
+
+  checkEmail: function() {
+    // 验证邮箱
+  },
+
+  checkPassword: function() {
+    // 验证密码
+  }
+};
+
+
+var ck = check.checkName();
+console.log( ck );
---
+++ chapter 1/2.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var check = function() {
+  return {
+    checkName: function() {
+      // 验证姓名
+    },
+
+    checkEmail: function() {
+      // 验证邮箱
+    },
+
+    checkPassword: function() {
+      // 验证密码
+    }
+  }
+};
+
+var a = check();
+a.checkName();
