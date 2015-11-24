be974ad8db8b1e65c3924b9af6052730d0db3ddf
berwin <berwin1995@qq.com>
2015-11-24 18:56:26 +0800

    update

--- chapter 1/5.js
+++ chapter 1/5.js
@@ -1,2 +1,26 @@
 'use strict';
 
+var Check = function() {};
+
+Check.prototype = {
+  checkName: function () {
+    console.log('校验姓名');
+
+    return this;
+  },
+
+  checkEmail: function() {
+    console.log('校验邮箱');
+
+    return this;
+  },
+
+  checkPassword: function() {
+    console.log('校验密码');
+
+    return this;
+  }
+};
+
+var a = new Check();
+a.checkName().checkEmail().checkPassword();
