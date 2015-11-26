9ea0380d9f54bd52a27da281c244e1ad91d73f95
berwin <berwin1995@qq.com>
2015-11-26 18:41:19 +0800

    update

---
+++ chapter2/3.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var SuperClass = function() {
+  this.superValue = true;
+};
+
+// 为父类添加公有方法
+SuperClass.prototype.getSuperValue = function() {
+  return this.superValue;
+};
+
+var SubClass = function() {
+  this.subValue = false;
+};
+
+// 继承父类
+SubClass.prototype = new SuperClass();
+
+// 为子类添加公有方法
+SubClass.prototype.getSubValue = function() {
+  return this.subValue;
+};
+
+var a = new SubClass();
+
+console.log('a.getSuperValue() =', a.getSuperValue()); // true
+console.log('a.getSubValue() =', a.getSubValue()); // false
+console.log('a.superValue =', a.superValue); // true
+console.log('a.subValue =', a.subValue); // false
+
+console.log('a instanceof SubClass =', a instanceof SubClass); // true
+console.log('a instanceof SuperClass =', a instanceof SuperClass); // true
+console.log('SubClass instanceof SuperClass =', SubClass instanceof SuperClass); // false
+console.log('SubClass.prototype instanceof SuperClass =', SubClass.prototype instanceof SuperClass); // true
