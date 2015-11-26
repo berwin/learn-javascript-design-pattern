fbadf05c6990633f76178a1fb6339df2d8152d64
berwin <berwin1995@qq.com>
2015-11-26 18:17:28 +0800

    update

--- chapter2/2.js
+++ chapter2/2.js
@@ -1,2 +1,15 @@
 'use strict';
 
+var Book = function(title, time, type) {
+  if (this instanceof Book) {
+    this.title = title;
+    this.time = time;
+    this.type = type;
+  } else {
+    return new Book(title, time, type);
+  }
+};
+
+var book = Book('Javascript', '2015', 'js');
+
+console.log(book);
